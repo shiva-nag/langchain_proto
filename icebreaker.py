b7ab05c604725f056db6494133a7118fec44f9e4
shankshivash <shnkrshvng@gmail.com>
2025-03-01 18:40:02 +0000

    Refreshing git repo

--- icebreaker.py
+++ icebreaker.py
@@ -1,17 +1,19 @@
+from typing import Tuple
+
 from dotenv import load_dotenv
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 
-from output_parser import summary_parser
+from output_parser import summary_parser, Summary
 #from langchain_ollama import ChatOllama
 
 from third_parties.linkedin_py import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
-def ice_break_with(name: str) -> str:
+def ice_break_with(name: str) -> Tuple[Summary,str]:
     linkedin_url = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(linked_profile_url=linkedin_url, mock=True)
+    linkedin_data = scrape_linkedin_profile(linked_profile_url=linkedin_url)
 
     summary_template = """
         given the LinkedIn information {information} about a person from, I want you to create:
@@ -31,9 +33,9 @@ def ice_break_with(name: str) -> str:
    # chain = summary_prompt_template | llm | StrOutputParser()
     chain = summary_prompt_template | llm | summary_parser
 #    linkedin_data = scrape_linkedin_profile(linked_profile_url="https://www.linkedin.com/in/shivashank/")
-    res = chain.invoke(input={"information": linkedin_data})
+    res:Summary = chain.invoke(input={"information": linkedin_data})
 
-    print(res)
+    return res, linkedin_data.get("profile_pic_url")
 
 
 if __name__ == "__main__":
